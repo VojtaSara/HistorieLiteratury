7a3433466e7f28a2ce0796b8a469bc96ffbd522d
VojtaSara <46105170+VojtaSara@users.noreply.github.com>
2019-01-06 13:51:16 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ var b;
 var taticek;
 var mezery = 25;
 var momentalniAutor = 0;
+var sloup = 150;
 
 function preload() {
   masterList = loadJSON("https://raw.githubusercontent.com/VojtaSara/HistorieLiteratury/master/seznam2.json");
@@ -11,15 +12,20 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(window.innerWidth,window.innerHeight);
+  createCanvas(windowWidth,windowHeight);
   //noCanvas();
   //background(255);
   gameMode = 0;
   displayAutor(0);
-  b1 = new button(width/2,200,"Zobrazit přehled autorů");
-  b2 = new button(width/2,280,"Přiřaď autory k dílům");
-  b3 = new button(width/2,360,"Uhádni dílo");
-  b4 = new button(width/2,height - 100,"Zpět");
+  b1 = new button(200,"Zobrazit přehled autorů");
+  b2 = new button(280,"Přiřaď autory k dílům");
+  b3 = new button(360,"Uhádni dílo");
+  b4 = new button(800,"Zpět");
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  b1.updatePos();
 }
 
 function draw() {
@@ -31,7 +37,7 @@ function draw() {
   }
   strokeWeight(1);
   fill(255,210);
-  rect(width/4,0,width*(1/2),height);
+  rect(sloup,0,width - 2*sloup,height);
   fill(0);
 
   if (gameMode == 0) {
@@ -50,10 +56,10 @@ function draw() {
     b4.show();
     strokeWeight(15);
     stroke(255);
-    line(width/4 - 50, height/2, width/4 - 20, height/2-30);
-    line(width/4 - 50, height/2, width/4 - 20, height/2+30);
-    line(width*3/4 + 50, height/2, width*3/4 + 20, height/2-30);
-    line(width*3/4 + 50, height/2, width*3/4 + 20, height/2+30);
+    line(sloup - 50, height/2, sloup - 20, height/2-30);
+    line(sloup - 50, height/2, sloup - 20, height/2+30);
+    line(width - sloup + 50, height/2, width - sloup + 20, height/2-30);
+    line(width - sloup + 50, height/2, width - sloup + 20, height/2+30);
     stroke(0);
     strokeWeight(1);
   } else if (gameMode == 2) {
@@ -77,38 +83,37 @@ function mousePressed() {
       gameMode = 2;
     }
   } else if (gameMode == 1) {
-    if (mouseX < width/4 && momentalniAutor > 0) {
+    if (mouseX < sloup && momentalniAutor > 0) {
       momentalniAutor--;
-    } else if (mouseX > width*3/4 && momentalniAutor < masterList.autori.length - 1) {
+    } else if (mouseX > width - sloup && momentalniAutor < masterList.autori.length - 1) {
       momentalniAutor++;
     }
-    if (mouseX > width/2-150 && mouseX < width/2 + 150 && mouseY > height - 130 && mouseY < height - 70) {
+    if (mouseX > width/2-150 && mouseX < width/2 + 150 && mouseY > b4.y - 30 && mouseY < b4.y + 30) {
       gameMode = 0;
     }
   } else if (gameMode == 2 || gameMode == 3) {
-    if (mouseX > width/2-150 && mouseX < width/2 + 150 && mouseY > height - 130 && mouseY < height - 70) {
+    if (mouseX > width/2-150 && mouseX < width/2 + 150 && mouseY > b4.y - 30 && mouseY < b4.y + 30) {
       gameMode = 0;
     }
   }
 }
 
 class button {
-  constructor(x_, y_, text_) {
-    this.x = x_;
+  constructor(y_, text_) {
     this.y = y_;
     this.text = text_;
   }
   show() {
     stroke(1);
     fill(255);
-    if (mouseX > this.x-150 && mouseX < this.x + 150 && mouseY > this.y - 30 && mouseY < this.y + 30) {
+    if (mouseX > width/2-150 && mouseX < width/2 + 150 && mouseY > this.y - 30 && mouseY < this.y + 30) {
       fill(220,220,220);
     }
     textSize(20);
-    rect(this.x-150,this.y-30,300,60);
+    rect(width/2-150,this.y-30,300,60);
     fill(0);
     textAlign(CENTER, CENTER);
-    text(this.text,this.x,this.y);
+    text(this.text,width/2,this.y);
   }
 }
 
@@ -123,29 +128,29 @@ function displayAutor(num) {
   text(masterList.autori[num].Narozeni + " - " + masterList.autori[num].Umrti, width/2, 115 + 0*mezery);
   textAlign(LEFT);
   textStyle(BOLD);
-  text("Národnost: ", width/4 + 40, 115 + 2*mezery);
+  text("Národnost: ", sloup + 40, 115 + 2*mezery);
   textStyle(NORMAL);
-  text(masterList.autori[num].Narodnost, width/4 + 130, 115 + 2*mezery);
+  text(masterList.autori[num].Narodnost, sloup + 130, 115 + 2*mezery);
   textStyle(BOLD);
-  text("Umělecký směr: ", width/4 + 40, 115 + 3*mezery);
+  text("Umělecký směr: ", sloup + 40, 115 + 3*mezery);
   textStyle(NORMAL);
-  text(masterList.autori[num].Styl, width/4 + 170, 115 + 3*mezery);
+  text(masterList.autori[num].Styl, sloup + 170, 115 + 3*mezery);
   textStyle(BOLD);
-  text("Povolání: ", width/4 + 40, 115 + 4*mezery);
+  text("Povolání: ", sloup + 40, 115 + 4*mezery);
   textStyle(NORMAL);
-  text(masterList.autori[num].Povolani, width/4 + 115, 115 + 4*mezery);
+  text(masterList.autori[num].Povolani, sloup + 115, 115 + 4*mezery);
   textStyle(BOLD);
-  text("Žánry: ", width/4 + 40, 115 + 5*mezery);
+  text("Žánry: ", sloup + 40, 115 + 5*mezery);
   textStyle(NORMAL);
-  text(masterList.autori[num].Zanry, width/4 + 90, 115 + 5*mezery);
+  text(masterList.autori[num].Zanry, sloup + 90, 115 + 5*mezery);
   textStyle(BOLD);
-  text("Popis: ", width/4 + 40, 115 + 6*mezery);
+  text("Popis: ", sloup + 40, 115 + 6*mezery);
   textStyle(NORMAL);
-  text(masterList.autori[num].Zivotopis, width/4 + 90, 115 + 6*mezery);
+  text(masterList.autori[num].Zivotopis, sloup + 90, 115 + 6*mezery);
   textStyle(BOLD);
-  text("Dílo: ", width/4 + 40, 115 + 7*mezery);
+  text("Dílo: ", sloup + 40, 115 + 7*mezery);
   textStyle(NORMAL);
   for (var i = 0; i < masterList.autori[num].Dila.length; i ++) {
-    text(masterList.autori[num].Dila[i].NazevDila + " (" + masterList.autori[num].Dila[i].RokVydani + ") \n\t\t" + masterList.autori[num].Dila[i].PopisDila, width/4 + 60, 120 + 8*mezery + i*50);
+    text(masterList.autori[num].Dila[i].NazevDila + " (" + masterList.autori[num].Dila[i].RokVydani + ") \n\t\t" + masterList.autori[num].Dila[i].PopisDila, sloup + 60, 120 + 8*mezery + i*50);
   }
 }
